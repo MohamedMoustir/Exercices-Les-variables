228040e424bb6b2beec3c9aa0673dd17433fe4ff
MohamedMoustir <itsmoustir@gmail.com>
2024-10-12 18:04:13 +0100

    Add Exercice 4 in file js

--- main.js
+++ main.js
@@ -13,9 +13,21 @@ console.log(a-b);
 console.log(a/b);
 console.log(a%b);
 console.log("################################################");
+
 // Exercice 3 : Concaténation de chaînes de caractères
 let nom = "MOUSTIR";
 let prenom1 = "MOHAMED";
 let full_name = prenom1 +' '+nom ;
 console.log(full_name);
+console.log("################################################");
+
+// Exercice 4 : Utilisation des variables booléennes
+let age = 16;
+let majeur = true;
+if(age >= 18){
+ majeur = true;
+}else{
+    majeur = false;
+}
+console.log(majeur);
 console.log("################################################");

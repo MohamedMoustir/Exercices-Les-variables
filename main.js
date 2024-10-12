428a619b36e90ee9095b4d6be4cf5524bea67a25
MohamedMoustir <itsmoustir@gmail.com>
2024-10-12 17:47:42 +0100

    update fil main.js

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-//Déclaration et affectation de variables
+// Exercice 1 : Déclaration et affectation de variables
 let prenom = "MOHAMED";
 console.log(prenom);
+console.log("################################################");
+
+//Exercice 2 : Manipulation de variables numériques
+
+let a = 4;
+let b = 6;
+console.log(a+b);
+console.log(a*b);
+console.log(a-b);
+console.log(a/b);
+console.log(a%b);
+
